66692d6ad17c35ef77bd9f5f85753d4df7a4bb38
Kewan Shunn <kewanshunn@gmail.com>
2014-06-26 14:59:00 +0100

    Change dir to morphs

--- lib/meta_morpha/rails.rb
+++ lib/meta_morpha/rails.rb
@@ -1,5 +1,5 @@
 module MetaMorpha
   class Rails < ::Rails::Engine
-    config.autoload_paths << File.expand_path("#{config.root}/app/formatters") if File.exist?("#{config.root}/app/formatters")
+    config.autoload_paths << File.expand_path("#{config.root}/app/morphs") if File.exist?("#{config.root}/app/formatters")
   end if defined?(::Rails)
 end
